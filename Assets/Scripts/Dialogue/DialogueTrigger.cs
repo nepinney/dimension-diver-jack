fab8284e87fed31b711bba1d46c8023541f3254b
dathanki <dathanki@lakeheadu.ca>
2021-11-22 05:32:42 -0500

    .test

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour {
 
 	public Dialogue dialogue;
-
+	
 	public void TriggerDialogue ()
 	{
 		FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+		// add trigger using a box collider for text area to pop up
 	}
-
 }
